c9c94d82950a820f869c5154affb4ae25cbfd6c4
Vitaliy Petriv <vamnir132132@gmail.com>
2022-09-07 22:18:15 +0300

    Create App template

---
+++ Tips/yarn.md
@@ -0,0 +1,2 @@
+If in one moment you fix bag with yarnt error
+<!-- "ci": "rm -rf node_modules && yarn install", -->
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export const App = () => {
+	return <h1 className='p-3'>React TypeScript Webpack starter template</h1>
+}

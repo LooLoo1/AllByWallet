300e1d75b39e0ba3ff66fae2ac009262942b2959
Vitaliy Petriv <vamnir132132@gmail.com>
2022-09-09 18:16:52 +0300

    [Nav] Add nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import Header from './components/Header'
+import Nav from './components/Nav'
 
 export const App = () => {
 	return (
-		<div className='bg-gray w-[428px] mx-auto min-h-screen scroll-smooth'>
+		<div className='bg-gray w-[428px] mx-auto min-h-screen scroll-smooth relative'>
 			<Header/>
+			<Nav/>
 		</div>
 	)
 }
---
+++ src/components/Nav.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default function Currency() {
+
+  return (
+	 <nav className='fixed w-[inherit] bottom-0 bg-white rounded-t-3xl'>
+		Test
+	 </nav>
+  )
+}
